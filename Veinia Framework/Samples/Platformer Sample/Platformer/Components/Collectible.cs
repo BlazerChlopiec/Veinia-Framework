2fd826dc80bff4e43484528518005ad6d2b2b796
BlazerChlopiec <blazer8002@gmail.com>
2023-11-12 12:33:35 +0100

    Particles Added To Platformer

--- Veinia Framework/Samples/Platformer Sample/Platformer/Components/Collectible.cs
+++ Veinia Framework/Samples/Platformer Sample/Platformer/Components/Collectible.cs
@@ -1,18 +1,75 @@
-﻿using tainicom.Aether.Physics2D.Dynamics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Particles;
+using MonoGame.Extended.Particles.Modifiers;
+using MonoGame.Extended.Particles.Modifiers.Interpolators;
+using MonoGame.Extended.Particles.Profiles;
+using MonoGame.Extended.TextureAtlases;
+using System;
+using System.Collections.Generic;
+using tainicom.Aether.Physics2D.Dynamics;
 using tainicom.Aether.Physics2D.Dynamics.Contacts;
 
 namespace VeiniaFramework.Samples.Platformer
 {
 	public class Collectible : Component
 	{
+		ParticleEffect collectParticles;
+
 		public override bool OnCollide(Fixture sender, Fixture other, Contact contact)
 		{
 			var tag = (Player)other.Body.Tag;
-			if (tag != null) Collect();
+			if (tag != null && collectParticles == null) Collect();
 
 			return true;
 		}
 
-		private void Collect() => DestroyGameObject();
+		private void Collect()
+		{
+			CollectParticles();
+			DestroyGameObject();
+		}
+
+		private void CollectParticles()
+		{
+			if (collectParticles == null)
+			{
+				var texture = new Texture2D(Globals.graphicsDevice, 1, 1);
+				texture.SetData(new[] { Color.White });
+
+				collectParticles = Globals.particleWorld.Add(new ParticleEffect()
+				{
+					Emitters = new List<ParticleEmitter>
+				{
+					new ParticleEmitter(new TextureRegion2D(texture), 100, TimeSpan.FromSeconds(.4f),
+						Profile.BoxFill(50, 50))
+					{
+						AutoTrigger=false,
+						Parameters = new ParticleReleaseParameters
+						{
+							Speed = new Range<float>(300, 500),
+							Rotation = new Range<float>(-1f, 1f),
+							Quantity = 4,
+						},
+						Modifiers =
+						{
+							new AgeModifier
+							{
+								Interpolators =
+								{
+									new ColorInterpolator {  StartValue = new HslColor(56, 1f, .5f), EndValue = new HslColor(56, 1f, .5f) },
+									new OpacityInterpolator{ StartValue = 1, EndValue = 0 },
+									new ScaleInterpolator { StartValue = new Vector2(50,50), EndValue = Vector2.Zero },
+								}
+							},
+							new RotationModifier {RotationRate = -2.1f},
+						}
+					}
+				},
+				});
+			}
+			collectParticles.Trigger(transform.screenPos, layerDepth: .3f);
+		}
 	}
 }
--- Veinia Framework/Samples/Platformer Sample/Platformer/Components/Player.cs
+++ Veinia Framework/Samples/Platformer Sample/Platformer/Components/Player.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
+using MonoGame.Extended.Particles;
+using MonoGame.Extended.Particles.Modifiers;
+using MonoGame.Extended.Particles.Modifiers.Interpolators;
+using MonoGame.Extended.Particles.Profiles;
+using MonoGame.Extended.TextureAtlases;
 using System;
+using System.Collections.Generic;
 using tainicom.Aether.Physics2D.Dynamics;
 using tainicom.Aether.Physics2D.Dynamics.Contacts;
 
@@ -15,6 +22,8 @@ namespace VeiniaFramework.Samples.Platformer
 
 		Fixture groundCheck;
 		int groundOverlaps;
+
+		ParticleEffect jumpParticles;
 
 		bool isTouchingGround => groundOverlaps >= 1;
 
@@ -49,6 +58,7 @@ namespace VeiniaFramework.Samples.Platformer
 
 			if ((Globals.input.GetKeyButton(Keys.Space, Buttons.A) || Globals.input.GetKey(Keys.W)) && isTouchingGround)
 			{
+				JumpParticles();
 				body.LinearVelocity = new Vector2(body.LinearVelocity.X, jumpForce);
 			}
 			if ((Globals.input.GetKeyButtonUp(Keys.Space, Buttons.A) || Globals.input.GetKeyUp(Keys.W)) && body.LinearVelocity.Y > 0)
@@ -63,6 +73,48 @@ namespace VeiniaFramework.Samples.Platformer
 			float zoomSpeed = .8f;
 			smoothZoom = MathHelper.Lerp(smoothZoom, (MathF.Abs(smoothHorizontal) / zoomAmount), zoomSpeed * Time.deltaTime);
 			Globals.camera.Scale = Vector2.One * (1f - smoothZoom);
+		}
+
+		private void JumpParticles()
+		{
+			if (jumpParticles == null)
+			{
+				var texture = new Texture2D(Globals.graphicsDevice, 1, 1);
+				texture.SetData(new[] { Color.White });
+
+				jumpParticles = Globals.particleWorld.Add(new ParticleEffect()
+				{
+					Emitters = new List<ParticleEmitter>
+				{
+					new ParticleEmitter(new TextureRegion2D(texture), 100, TimeSpan.FromSeconds(.5f),
+						Profile.BoxFill(50, 50))
+					{
+						AutoTrigger=false,
+						Parameters = new ParticleReleaseParameters
+						{
+							Speed = new Range<float>(150, 300),
+							Rotation = new Range<float>(-1f, 1f),
+							Quantity = 4,
+						},
+						Modifiers =
+						{
+							new AgeModifier
+							{
+								Interpolators =
+								{
+									new ColorInterpolator {  StartValue = new HslColor(74, .9f, .5f), EndValue = new HslColor(130, .41f, .5f) },
+									new OpacityInterpolator{ StartValue = 1, EndValue = 0 },
+									new ScaleInterpolator { StartValue = new Vector2(20,20), EndValue = Vector2.Zero },
+								}
+							},
+							new RotationModifier {RotationRate = -2.1f},
+							new LinearGravityModifier {Direction = -Vector2.UnitY, Strength = -1200},
+						}
+					}
+				},
+				});
+			}
+			jumpParticles.Trigger(transform.screenPos, layerDepth: .3f);
 		}
 
 		public override bool OnCollide(Fixture sender, Fixture other, Contact contact)
