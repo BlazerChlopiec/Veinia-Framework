ced6c00895d2627574ed3cfc435e0328602496cc
BlazerChlopiec <blazer8002@gmail.com>
2024-10-10 17:55:14 +0200

    Editor Shortcuts Added To Text Fields

--- Veinia Framework/Veinia/Source/Editor/Scripts/Components/Toolbars/EditToolbar.cs
+++ Veinia Framework/Veinia/Source/Editor/Scripts/Components/Toolbars/EditToolbar.cs
@@ -15,28 +15,28 @@ namespace VeiniaFramework.Editor
 			var panel = new Panel { Height = 100 };
 			finalToolbarContent = panel;
 
-			var removeSelectedButton = new TextButton { Text = "Remove Selected" };
+			var removeSelectedButton = new TextButton { Text = "Remove Selected [RMB]" };
 			removeSelectedButton.Click += (o, e) =>
 			{
 				editToolbarBehaviour.RemoveSelection();
 			};
 			panel.Widgets.Add(removeSelectedButton);
 
-			var deselectButton = new TextButton { Text = "Deselect", Top = 25 };
+			var deselectButton = new TextButton { Text = "Deselect [LAlt+D]", Top = 25 };
 			deselectButton.Click += (o, e) =>
 			{
 				editToolbarBehaviour.selectedObjects.Clear();
 			};
 			panel.Widgets.Add(deselectButton);
 
-			var duplicateButton = new TextButton { Text = "Duplicate", Top = 50 };
+			var duplicateButton = new TextButton { Text = "Duplicate [Ctrl+D]", Top = 50 };
 			duplicateButton.Click += (o, e) =>
 			{
 				editToolbarBehaviour.Duplicate();
 			};
 			panel.Widgets.Add(duplicateButton);
 
-			var filterSelectionButton = new TextButton { Text = "Filter Selection", Top = 75 };
+			var filterSelectionButton = new TextButton { Text = "Filter Selection [Q]", Top = 75 };
 			filterSelectionButton.Click += (o, e) =>
 			{
 				editToolbarBehaviour.SelectionOverlapWindow();
