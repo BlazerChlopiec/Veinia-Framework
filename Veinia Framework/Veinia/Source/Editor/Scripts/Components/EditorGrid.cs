47fe42f76199ecd08e80f888082d92f1d1843b86
BlazerChlopiec <blazer8002@gmail.com>
2025-08-04 15:48:26 +0200

    Grid Thickness Scales With Camera Zoom

--- Veinia Framework/Veinia/Source/Editor/Scripts/Components/EditorGrid.cs
+++ Veinia Framework/Veinia/Source/Editor/Scripts/Components/EditorGrid.cs
@@ -21,24 +21,24 @@ namespace VeiniaFramework.Editor
 			var camera = Globals.camera;
 			var unit = Transform.unitSize;
 			var size = camera.GetUnitsInView();
+			var color = Color.White * opacity;
+			var thickness = 2 / Globals.camera.GetScale();
 
 			var leftBottom = Transform.WorldToScreenPos(Vector2.Round(camera.GetPosition() - Vector2.UnitX * (size.X / 2) - Vector2.UnitY * (size.Y / 2)) - Vector2.UnitX / 2);
 			for (int i = 0; i < MathF.Round(size.Y) + 1; i++)
 			{
 				var yOffset = (-Vector2.UnitY * unit * i) + -Vector2.UnitY * unit / 2;
 				var xOffset = (Vector2.UnitX * unit * (size.X + 1));
-				sb.DrawLine(leftBottom + yOffset, leftBottom + xOffset + yOffset, Color.White * opacity, 2, layerDepth: .9f);
+				sb.DrawLine(leftBottom + yOffset, leftBottom + xOffset + yOffset, color, thickness, layerDepth: .9f);
 			}
-			//sb.DrawRectangle(new RectangleF(leftBottom.X, leftBottom.Y, unit, unit), Color.Red, 5, 1);
 
 			var rightTop = Transform.WorldToScreenPos(Vector2.Round(camera.GetPosition() + Vector2.UnitX * (size.X / 2) + Vector2.UnitY * (size.Y / 2)) + Vector2.UnitY / 2);
 			for (int i = 0; i < MathF.Round(size.X) + 1; i++)
 			{
 				var xOffset = (-Vector2.UnitX * unit * i) + -Vector2.UnitX * unit / 2;
 				var yOffset = (Vector2.UnitY * unit * (size.Y + 1));
-				sb.DrawLine(rightTop + xOffset, rightTop + xOffset + yOffset, Color.White * opacity, 2, layerDepth: .9f);
+				sb.DrawLine(rightTop + xOffset, rightTop + xOffset + yOffset, color, thickness, layerDepth: .9f);
 			}
-			//sb.DrawRectangle(new RectangleF(rightTop.X, rightTop.Y, unit, unit), Color.Red, 5, 1);
 		}
 	}
 }
