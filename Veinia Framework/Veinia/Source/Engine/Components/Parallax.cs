85327ec09d9c7245b02e969442ffa7f3c5e8021b
BlazerChlopiec <blazer8002@gmail.com>
2025-12-17 20:19:37 +0100

    Parallax Added To Namespace

--- Veinia Framework/Veinia/Source/Engine/Components/Parallax.cs
+++ Veinia Framework/Veinia/Source/Engine/Components/Parallax.cs
@@ -2,99 +2,101 @@ using Apos.Camera;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using VeiniaFramework;
 
-public class Parallax : Sprite
+namespace VeiniaFramework
 {
-	public Parallax(Texture2D texture, Color color, float pixelsPerUnit,
-	 float factorX = .5f, float factorY = .5f, int copiesX = 1, int copiesY = 1, Vector2 scrollSpeed = default) : base(texture, color, pixelsPerUnit)
+	public class Parallax : Sprite
 	{
-		this.factorX = factorX;
-		this.factorY = factorY;
-		this.copiesX = copiesX;
-		this.copiesY = copiesY;
-		this.scrollSpeed = scrollSpeed;
-	}
-
-	public Parallax(string path, Color color, float pixelsPerUnit,
-	 float factorX = .5f, float factorY = .5f, int copiesX = 1, int copiesY = 1, Vector2 scrollSpeed = default) : base(path, color, pixelsPerUnit)
-	{
-		this.factorX = factorX;
-		this.factorY = factorY;
-		this.copiesX = copiesX;
-		this.copiesY = copiesY;
-		this.scrollSpeed = scrollSpeed;
-	}
-
-	float factorX;
-	float factorY;
-	public int copiesX; // 1 - one sprite copy for left and right
-	public int copiesY; // 1 - one sprite copy for up and down
-
-	Vector2 scrollSpeed; // auto scrolling
-
-	Vector2 lastCameraPos;
-	Camera cam;
-	float textureUnitSize;
-
-
-	public override void EarlyInitialize()
-	{
-		cam = Globals.camera;
-		lastCameraPos = cam.GetPosition();
-		textureUnitSize = rect.Width / Transform.unitSize;
-	}
-
-	public override void Update()
-	{
-		// this script runs in the editor since it inherits Sprite
-		if (Veinia.isEditor) return;
-
-		var camPos = cam.GetPosition();
-
-		var deltaMovement = camPos - lastCameraPos;
-		transform.position += new Vector2(deltaMovement.X * factorX, deltaMovement.Y * factorY) + (scrollSpeed * Time.deltaTime);
-
-		lastCameraPos = camPos;
-
-
-		if (MathF.Abs(camPos.X - transform.position.X) >= textureUnitSize)
+		public Parallax(Texture2D texture, Color color, float pixelsPerUnit,
+		 float factorX = .5f, float factorY = .5f, int copiesX = 1, int copiesY = 1, Vector2 scrollSpeed = default) : base(texture, color, pixelsPerUnit)
 		{
-			var offsetPosX = (camPos.X - transform.position.X) % textureUnitSize;
-			transform.position = new Vector2(camPos.X + offsetPosX, transform.position.Y);
+			this.factorX = factorX;
+			this.factorY = factorY;
+			this.copiesX = copiesX;
+			this.copiesY = copiesY;
+			this.scrollSpeed = scrollSpeed;
 		}
 
-		if (MathF.Abs(camPos.Y - transform.position.Y) >= textureUnitSize)
+		public Parallax(string path, Color color, float pixelsPerUnit,
+		 float factorX = .5f, float factorY = .5f, int copiesX = 1, int copiesY = 1, Vector2 scrollSpeed = default) : base(path, color, pixelsPerUnit)
 		{
-			var offsetPosY = (camPos.Y - transform.position.Y) % textureUnitSize;
-			transform.position = new Vector2(transform.position.X, camPos.Y + offsetPosY);
+			this.factorX = factorX;
+			this.factorY = factorY;
+			this.copiesX = copiesX;
+			this.copiesY = copiesY;
+			this.scrollSpeed = scrollSpeed;
 		}
-	}
 
-	public override void Draw(SpriteBatch sb)
-	{
-		for (int x = -copiesX; x <= copiesX; x++)
+		float factorX;
+		float factorY;
+		public int copiesX; // 1 - one sprite copy for left and right
+		public int copiesY; // 1 - one sprite copy for up and down
+
+		Vector2 scrollSpeed; // auto scrolling
+
+		Vector2 lastCameraPos;
+		Camera cam;
+		float textureUnitSize;
+
+
+		public override void EarlyInitialize()
 		{
-			for (int y = -copiesY; y <= copiesY; y++)
+			cam = Globals.camera;
+			lastCameraPos = cam.GetPosition();
+			textureUnitSize = rect.Width / Transform.unitSize;
+		}
+
+		public override void Update()
+		{
+			// this script runs in the editor since it inherits Sprite
+			if (Veinia.isEditor) return;
+
+			var camPos = cam.GetPosition();
+
+			var deltaMovement = camPos - lastCameraPos;
+			transform.position += new Vector2(deltaMovement.X * factorX, deltaMovement.Y * factorY) + (scrollSpeed * Time.deltaTime);
+
+			lastCameraPos = camPos;
+
+
+			if (MathF.Abs(camPos.X - transform.position.X) >= textureUnitSize)
 			{
-				Vector2 offset = new Vector2(rect.Width * x, rect.Height * y);
-				DrawParallax(sb, offset);
+				var offsetPosX = (camPos.X - transform.position.X) % textureUnitSize;
+				transform.position = new Vector2(camPos.X + offsetPosX, transform.position.Y);
+			}
+
+			if (MathF.Abs(camPos.Y - transform.position.Y) >= textureUnitSize)
+			{
+				var offsetPosY = (camPos.Y - transform.position.Y) % textureUnitSize;
+				transform.position = new Vector2(transform.position.X, camPos.Y + offsetPosY);
 			}
 		}
-	}
 
-	private void DrawParallax(SpriteBatch sb, Vector2 offset = default)
-	{
-		level.drawCommands.Add(new DrawCommand
+		public override void Draw(SpriteBatch sb)
 		{
-			command = delegate
+			for (int x = -copiesX; x <= copiesX; x++)
 			{
-				sb.Draw(texture, rect.OffsetNew(offset), null, color, MathHelper.ToRadians(transform.rotation),
-					 new Vector2(texture.Bounds.Width / 2, texture.Bounds.Height / 2),
-					 SpriteEffects.None, layerDepth: 0);
-			},
-			Z = transform.Z,
-			drawOptions = drawOptions
-		});
+				for (int y = -copiesY; y <= copiesY; y++)
+				{
+					Vector2 offset = new Vector2(rect.Width * x, rect.Height * y);
+					DrawParallax(sb, offset);
+				}
+			}
+		}
+
+		private void DrawParallax(SpriteBatch sb, Vector2 offset = default)
+		{
+			level.drawCommands.Add(new DrawCommand
+			{
+				command = delegate
+				{
+					sb.Draw(texture, rect.OffsetNew(offset), null, color, MathHelper.ToRadians(transform.rotation),
+						 new Vector2(texture.Bounds.Width / 2, texture.Bounds.Height / 2),
+						 SpriteEffects.None, layerDepth: 0);
+				},
+				Z = transform.Z,
+				drawOptions = drawOptions
+			});
+		}
 	}
 }
