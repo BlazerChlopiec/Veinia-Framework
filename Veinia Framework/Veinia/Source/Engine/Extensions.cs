d2234a0fbbc910dd1f6c760ff07533dc2902d973
BlazerChlopiec <blazer8002@gmail.com>
2026-05-17 01:49:05 +0200

    Revert "Color ToColor & FSColor ToColor Extensions"
    This reverts commit e0caab63d1e32e7747a2bb765053f8e61d640d79.

--- Veinia Framework/Veinia/Source/Engine/Extensions.cs
+++ Veinia Framework/Veinia/Source/Engine/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using FontStashSharp;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -149,14 +148,6 @@ namespace VeiniaFramework
 		{
 			byte alpha = (byte)(MathHelper.Clamp(alpha01, 0f, 1f) * 255f);
 			return new Color(color.R, color.G, color.B, alpha);
-		}
-		public static Color ToColor(this FSColor fs)
-		{
-			return new Color(fs.PackedValue);
-		}
-		public static FSColor ToColor(this Color fs)
-		{
-			return new FSColor(fs.PackedValue);
 		}
 		public static Vector2 Round(this Vector2 vector, int decimalDigits)
 		{
