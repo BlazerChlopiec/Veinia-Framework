b97b8a400f089b4cbf6b874ffce035c830bb9a37
blazer2dev <blazer8002@gmail.com>
2026-07-18 21:09:00 +0200

    Fix Edit Multiple Objects In Release

--- Veinia Framework/Veinia/Source/Editor/Components/Toolbars/ToolbarBehaviour/EditToolbarBehaviour.cs
+++ Veinia Framework/Veinia/Source/Editor/Components/Toolbars/ToolbarBehaviour/EditToolbarBehaviour.cs
@@ -370,7 +370,7 @@ namespace VeiniaFramework.Editor
 				data.Color = first.Color;
 
 
-			editWindow = Globals.myraDesktop.MakeEditWindow(data, "Multiple Object Editor");
+			editWindow = Globals.myraDesktop.MakeEditWindow(data, "Multiple Object Editor", allowInReleaseMode: true);
 
 			PropertyGrid grid = (PropertyGrid)editWindow.Content;
 
